9ba35c816b469d5c4c8eb1dde2dddd0e9ee60cad
arieldev <arielferaguirre.2001@gmail.com>
2024-03-11 17:33:06 -0500

    improves

--- components/LocaleSwitcher.js
+++ components/LocaleSwitcher.js
@@ -1,16 +1,14 @@
 import { useRouter } from 'next/router'
 import Link from 'next/link'
-import { IconButton } from '@chakra-ui/react'
+import { Button } from '@chakra-ui/react'
 
 export function LocaleSwitcher() {
   const { locale, locales, route } = useRouter()
   const otherLocale = locales?.find(cur => cur !== locale)
   return (
-    <IconButton>
-      <Link href={route} locale={otherLocale}>
-        {`${otherLocale === 'en-US' ? '🇺🇸' : '🇪🇸'}`}
-      </Link>
-    </IconButton>
+    <Link href={route} locale={otherLocale}>
+      <Button>{`${otherLocale === 'en-US' ? '🇺🇸' : '🇪🇸'}`}</Button>
+    </Link>
   )
 }
 
